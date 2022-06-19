e90726a47fa84eeb58024878cf1d94ce2dc8dbd5
shintarou-akao <xftap2e4c4g@yahoo.co.jp>
2022-06-19 11:03:54 +0900

    reviewdogが機能するかテスト

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private function sample() {
+        echo $sample;
+    }
 }
